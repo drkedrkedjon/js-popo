ce05706d6fd7ce957e7a3b5c2336f66ac2ec5766
Sasha S.P. <sasa@bubulazy.com>
2026-03-09 18:48:03 +0100

    Hoisting

--- variables.js
+++ variables.js
@@ -1,2 +1,16 @@
-const miFunction = () => "sasa";
-console.log(miFunction());
+function pepes() {
+  return 1;
+}
+let sasa, pepe;
+console.log(juju);
+
+// sasa = "sasaahdfkjalhsdl";
+pepe = "asdasd"; // Test
+/*
+var juju = "sasa";
+console.log(juju);
+
+console.log(pepes()); 
+*/
+
+console.log(sasa);
