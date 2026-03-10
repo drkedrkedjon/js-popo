74ba1d5df51bcb77ef4a65d102184aefa66273e6
Sasha S.P. <sasa@bubulazy.com>
2026-03-10 17:40:02 +0100

    JS - Conditionals

--- conditionals.js
+++ conditionals.js
@@ -1,1 +1,31 @@
-console.log("sasa");
+const age = 128;
+
+if (age < 18) {
+  console.log("No puedes conducir");
+} else if ((age >= 80 && age <= 100) || age > 100) {
+  console.log("Eres un viejo carajillo");
+} else {
+  console.log("Dame tu tarjeta de credito");
+}
+
+switch (true) {
+  case age < 18:
+    console.log("No puedes conducir");
+    break;
+  case age >= 80:
+    console.log("Eres un viejo carajillo");
+    break;
+  default:
+    console.log("Dame tu tarjeta de credito");
+}
+
+switch (age) {
+  case 18:
+    console.log("Balonja");
+    break;
+  case 80:
+    console.log("Eres un viejo carajillo");
+    break;
+  default:
+    console.log("Who are you?");
+}
