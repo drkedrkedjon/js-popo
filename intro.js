59446448832b08c5b415c8b4b1c6f16af00618cf
Sasha S.P. <sasa@bubulazy.com>
2026-03-10 11:49:09 +0100

    JS - Intro - 2

---
+++ intro.js
@@ -0,0 +1,14 @@
+// Type check in JS
+let typeCheck = "Sasa";
+// console.log(typeof typeCheck);
+
+// Converting types
+console.log("100" + String(42));
+let sususu = 43;
+console.log(sususu.toString());
+
+var numerito = "4.45";
+console.log(parseInt(numerito)); // return 4
+console.log(parseFloat(numerito)); // return 4.45
+console.log(parseInt("622219677 es mi telefone")); // Saca 622219677 pero tiene que empezar por numero
+console.log(+"56"); // Convierte string into the number - new one for me
