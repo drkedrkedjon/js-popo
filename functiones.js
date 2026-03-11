a18993f8224ec4a510eaaf7be23841fc5a59d968
Sasha S.P. <sasa@bubulazy.com>
2026-03-11 13:05:57 +0100

    JS - Functions 5

--- functiones.js
+++ functiones.js
@@ -1,1 +1,38 @@
-console.log(null);
+let nombre = null;
+
+function noDefinida() {
+  nombre = "Caty";
+  return "Soy return";
+}
+
+const ret = noDefinida();
+
+console.log(nombre);
+console.log(ret);
+
+function strings() {
+  // Use slice to return a substring starting from the "w" through the end of the sentence
+  var str = "The five boxing wizards jump quickly";
+  const obj = {
+    stringi: str,
+  };
+  return obj.stringi.slice(16);
+}
+strings();
+console.log(strings());
+
+// Variable scope
+const user = "Sasa";
+
+function nombres() {
+  const user = "Caty";
+  return user;
+}
+console.log(nombres());
+console.log(user);
+
+function sasa(name = "Caty") {
+  console.log(name);
+}
+
+sasa();
