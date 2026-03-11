6e7ae8a361070e54475516ecf91e1d7457ed9984
Sasha S.P. <sasa@bubulazy.com>
2026-03-11 18:24:34 +0100

    JS - Arrays 2

--- arrays.js
+++ arrays.js
@@ -7,3 +7,20 @@ function trimer(arg) {
 }
 
 console.log(trimer(arr));
+
+const mixed = [
+  1,
+  "Hi",
+  [1, 2, 3, 4, 5],
+  { name: "Sasa" },
+  function hello() {
+    console.log("hello");
+  },
+];
+
+console.log(mixed[0]);
+console.log(mixed[1]);
+console.log(mixed[2]);
+console.log(mixed[2][1]);
+console.log(mixed[3].name);
+console.log(mixed[4]());
