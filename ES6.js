9b4d50e049a90163b9b55c0a944ffc1416a31985
Sasha S.P. <sasa@bubulazy.com>
2026-03-16 11:15:07 +0100

    ES6 destructuring 2

--- ES6.js
+++ ES6.js
@@ -41,10 +41,35 @@ console.log(one, two);
 // Destructuring in JS
 
 const arr2 = [1, 2, 3];
-const obj2 = { id: 23423, name: "Sasa", age: 45 };
+const obj2 = { id: 23423, nombre: "Sasa", age: 45 };
 
 const [dos, uno, cinco] = arr2;
 const { id, age, nombre } = obj2;
 
 console.log(uno, cinco, dos);
 console.log(id, nombre, age);
+
+function destructuracion({ id, nombre = "user", age }) {
+  console.log(id, nombre, age);
+}
+destructuracion(obj2);
+
+//  Spread y Rest operators with arrays
+let spreader = [1, 2, 3, 4, 5];
+let spreaderCopy = [...spreader];
+
+console.log(spreaderCopy === spreader);
+console.log(spreaderCopy);
+
+const [prm, seg, ...rest] = spreader;
+console.log(rest);
+
+// Spread and Rest with objects
+let spreadObj = { a: 1, b: 2, c: 3, d: 4 };
+let spreadObjCopy = { ...spreadObj };
+
+console.log(spreadObjCopy === spreadObj); // false
+console.log(spreadObjCopy); // { a: 1, b: 2, c: 3, d: 4 }
+
+const { a, d, ...restObj } = spreadObj;
+console.log(restObj); // { c: 3, d: 4 }
